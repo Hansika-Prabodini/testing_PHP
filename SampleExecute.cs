11bbd21e8a5f530c7f39ee5b2aded3e59fe48c74
Hansika-Prabodini <hansika@turintech.ai>
2025-03-17 12:59:50 +0530

    Add files via upload

---
+++ SampleExecute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CodeSamples
+{
+    public abstract class SampleExecute : ISampleExecute
+    {
+        private void PopColors()
+        {
+            Console.ResetColor();
+        }
+
+        private void SetTitleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        private void SetSectionColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        public void Title(string title)
+        {
+            SetTitleColor();
+            Console.WriteLine();
+            Console.WriteLine(title);
+            PopColors();
+            Console.WriteLine("============================================================");
+        }
+
+        public void Section(string title)
+        {
+            SetSectionColor();
+            Console.WriteLine(title);
+            PopColors();
+            Console.WriteLine("============================================================");
+        }
+
+        public void LineBreak()
+        {
+            Console.WriteLine("============================================================");
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        //from interface
+        //abstract methods MUST be implemented by derived class, because
+        //they do not have implementation in abstract class
+        public abstract void Execute();
+
+        //virtual methods do not have to be implemented by derived class, because
+        //they have implementation in abstract class; they can be overriden in derived
+        //class, if necessary
+        public virtual void ExecuteVirtual()
+        {
+            Console.WriteLine("Executing Virtual Method in abstract class");
+        }
+
+        //methods, that are not abstract or virtual, cannot be overriden in derived
+        //class
+        public void ExecuteNormal()
+        {
+            Console.WriteLine("Executing Method in abstract class");
+        }
+    }
+}
